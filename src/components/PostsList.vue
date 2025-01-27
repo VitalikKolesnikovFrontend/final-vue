fd1d5450dd51b0ac3d0d628a920176edff30b303
Vitalik <Kolesnikov.frontend@gmail.com>
2025-01-27 15:38:40 +0300

    deployed the project to vue and added post rendering

---
+++ src/components/PostsList.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({ title: String, content: String, image: String });
+</script>
+<template>
+  <div class="container">
+    <img :src="props.image" class="post-img" />
+    <h1>{{ props.title }}</h1>
+    <p>{{ props.content }}</p>
+  </div>
+</template>
+
+<style scoped>
+.post-img {
+  width: 500px;
+  height: 400px;
+}
+.container {
+  width: 500px;
+}
+</style>
