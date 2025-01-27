94e8e1a33da24785ddfa677541dd99f036c87dc1
Vitalik <Kolesnikov.frontend@gmail.com>
2025-01-27 17:49:24 +0300

    connected pinia

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import { Button } from "primevue";
+import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
 
 const app = createApp(App);
-app.mount("#app");
+const pinia = createPinia();
+app.use(pinia);
 app.use(PrimeVue);
 app.component(Button);
+app.mount("#app");
